1bd563359ebc54ec2d1b258d4a60e3f49925d6ab
Abraao Alves <abraao1231@gmail.com>
2021-04-12 22:59:13 -0300

    feat_alterações

--- src/users/views.py
+++ src/users/views.py
@@ -9,7 +9,8 @@ from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.settings import api_settings
 
-from rest_framework import permissions
+from rest_framework.viewsets import GenericViewSet
+from rest_framework import mixins, permissions
 
 
 
