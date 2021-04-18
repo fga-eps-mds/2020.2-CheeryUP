a6ef01da0d9a48f340020529c0bfc70cec791eb5
Durval Carvalho <dudurval2@gmail.com>
2021-04-17 22:33:45 -0300

    Modify paciente/views.py to use ModelViewSet

--- src/paciente/views.py
+++ src/paciente/views.py
@@ -1,33 +1,32 @@
-from django.shortcuts import render
-from rest_framework import viewsets, generics
-from rest_framework.views import APIView
+from rest_framework import viewsets
 from .models import Paciente
 from .serializers import PacienteSerializer
-from rest_framework.viewsets import GenericViewSet
-from rest_framework import mixins, permissions
-from rest_framework.decorators import api_view, permission_classes
 
-class PacienteViewSet(GenericViewSet, mixins.ListModelMixin, mixins.RetrieveModelMixin):
-    
+
+# class PacienteViewSet(GenericViewSet, mixins.ListModelMixin, mixins.RetrieveModelMixin):
+
+#     queryset = Paciente.objects.all()
+#     serializer_class = PacienteSerializer
+
+# class PacienteRegistrationAPIView(GenericViewSet, mixins.CreateModelMixin):
+#     serializer_class = PacienteSerializer
+#     queryset = Paciente.objects.all()
+
+# class PacienteDelete(GenericViewSet, mixins.DestroyModelMixin):
+
+#     serializer_class = PacienteSerializer
+#     queryset = Paciente.objects.all()
+#     lookup_field = 'cpf'
+
+# class PacienteUpdate(GenericViewSet, mixins.UpdateModelMixin):
+
+#     serializer_class = PacienteSerializer
+#     queryset = Paciente.objects.all()
+#     lookup_field = 'cpf'
+
+
+class PacienteModelViewSet(viewsets.ModelViewSet):
     queryset = Paciente.objects.all()
     serializer_class = PacienteSerializer
-
-class PacienteRegistrationAPIView(GenericViewSet, mixins.CreateModelMixin):
-
-    serializer_class = PacienteSerializer
-    queryset = Paciente.objects.all()
-
-class PacienteDelete(GenericViewSet, mixins.DestroyModelMixin):
-
-    serializer_class = PacienteSerializer
-    queryset = Paciente.objects.all()
     lookup_field = 'cpf'
-
-class PacienteUpdate(GenericViewSet, mixins.UpdateModelMixin):
-
-    serializer_class = PacienteSerializer
-    queryset = Paciente.objects.all()
-    lookup_field = 'cpf'
-       
-    
 
