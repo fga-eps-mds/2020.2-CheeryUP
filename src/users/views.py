58abef9bc94149d8ddbf8a846f8f6d7f8639cb2b
Durval Carvalho <dudurval2@gmail.com>
2021-04-17 22:31:19 -0300

    Modify users/views.py to use ModelViewSet

--- src/users/views.py
+++ src/users/views.py
@@ -1,39 +1,47 @@
-from django.core.checks.messages import Error
-from django.shortcuts import render
 from rest_framework import viewsets
+
+# from django.core.checks.messages import Error
+# from django.shortcuts import render
+# from django.contrib.auth.models import User
+
 from .models import Psicologo
-from django.contrib.auth.models import User
 from .serializers import PsicologoSerializer
 
-from rest_framework import status
-from rest_framework.response import Response
-from rest_framework.settings import api_settings
+# from rest_framework import status
+# from rest_framework.response import Response
+# from rest_framework.settings import api_settings
 
-from rest_framework.viewsets import GenericViewSet
-from rest_framework import mixins, permissions
-
-
-
-class PsicologoViewSet(GenericViewSet, mixins.ListModelMixin, mixins.RetrieveModelMixin):
-    queryset = Psicologo.objects.all()
-    serializer_class = PsicologoSerializer
-    permission_classes = (permissions.IsAuthenticated,)
+# from rest_framework.viewsets import GenericViewSet
+# from rest_framework import mixins, permissions
 
 
 
-class PsicologoRegistrationAPIView(GenericViewSet, mixins.CreateModelMixin):
-
-    serializer_class = PsicologoSerializer
-    queryset = Psicologo.objects.all()
-
-
-class PsicologoDelete(GenericViewSet, mixins.DestroyModelMixin):
-    serializer_class = PsicologoSerializer
-    queryset = Psicologo.objects.all()
-    lookup_field = 'nCRP'
+# class PsicologoViewSet(GenericViewSet, mixins.ListModelMixin, mixins.RetrieveModelMixin):
+#     queryset = Psicologo.objects.all()
+#     serializer_class = PsicologoSerializer
+#     permission_classes = (permissions.IsAuthenticated,)
 
 
-class PsicologoUpdate(GenericViewSet, mixins.UpdateModelMixin):
+
+# class PsicologoRegistrationAPIView(GenericViewSet, mixins.CreateModelMixin):
+
+#     serializer_class = PsicologoSerializer
+#     queryset = Psicologo.objects.all()
+
+
+# class PsicologoDelete(GenericViewSet, mixins.DestroyModelMixin):
+#     serializer_class = PsicologoSerializer
+#     queryset = Psicologo.objects.all()
+#     lookup_field = 'nCRP'
+
+
+# class PsicologoUpdate(GenericViewSet, mixins.UpdateModelMixin):
+#     serializer_class = PsicologoSerializer
+#     queryset = Psicologo.objects.all()
+#     lookup_field = 'nCRP'
+
+
+class PsicologoModelViewSet(viewsets.ModelViewSet):
     serializer_class = PsicologoSerializer
     queryset = Psicologo.objects.all()
     lookup_field = 'nCRP'
