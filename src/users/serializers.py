7b8b94e530e8d4f2ec609cbf62abe1351e33f76e
DaviMatheus <matdavimat@gmail.com>
2021-04-20 15:25:59 -0700

    att_mudança_user_serializer_validations

--- src/users/serializers.py
+++ src/users/serializers.py
@@ -2,20 +2,33 @@
 from rest_framework import serializers
 from .models import Psicologo
 from django.contrib.auth.models import User
-
+from rest_framework.validators import UniqueValidator
 from django.utils.translation import gettext
 
-class UserSerializer(serializers.ModelSerializer):
-    password = serializers.CharField(
+class UserSerializer(serializers.Serializer):
+    username = serializers.CharField(max_length=100)
+  
+    email = serializers.EmailField(
         required=True,
-        help_text='Leave empty if no change needed',
-        # style={'input_type': 'password', 'placeholder': 'Password'}
+        validators=[UniqueValidator(queryset=User.objects.all())],
+        # unique=True,
+        label="Email Address",
     )
+    password = serializers.CharField(
+        write_only=True,
+        required=True,
+        min_length=6,
+    )
+ 
     class Meta:
         model = User
         fields = ('username', 'email', 'password')
 
-
+    def validate_password(self, password):
+        if len(password) < 6:
+            raise serializers.ValidationError('Senha fraca!')
+            
+        return password
 
 class PsicologoSerializer(serializers.ModelSerializer):
     user = UserSerializer(required=True)
