bd00c1fde7aee829d63136c6489f8f3f813a0345
DaviMatheus <matdavimat@gmail.com>
2021-04-20 15:26:48 -0700

    att_tests_validations_users #112

--- src/users/tests/test_views.py
+++ src/users/tests/test_views.py
@@ -50,7 +50,7 @@ class PsicologoModelViewSetTestCase(APITestCase):
                 "email": "afonso@solano.com",
                 "password": "afonso_mg123"
             },
-            "nCRP": "1", # valid CPF
+            "nCRP": "1", # invalid CPF
             "bio": "Sou o afonso solano",
             "genero": "M"
         }
@@ -67,3 +67,61 @@ class PsicologoModelViewSetTestCase(APITestCase):
             qs.exists(),
             msg='Foi salvo no banco de dados um psicologo com CRP inválido'
         )
+
+    def test_unique_email_validation(self):
+        """
+        Test to try to create a user with
+        a registered username
+        """
+
+        self.data = {
+            "user": {
+                "username": "afonso",
+                "email": "afonso@solano.com",
+                "password": "123456"
+            },
+            "nCRP": "11111111111", # invalid CPF
+            "bio": "Sou o afonso solano",
+            "genero": "M"
+        }
+
+        request = self.factory.post(self.url_list, self.data, format='json')
+        response = self.view_list(request)
+        self.assertEqual(201, response.status_code)
+
+        user_data = self.data = {
+            "user": {
+                "username": "afonso",
+                "email": "afonso@solano.com",
+                "password": "123456"
+            },
+            "nCRP": "111111111112", 
+            "bio": "Sou o afonso solano",
+            "genero": "M"
+        }
+                
+        request = self.factory.post(self.url_list, self.data, format='json')
+        response = self.view_list(request)
+        self.assertEqual(400, response.status_code)
+
+    def test_sending_invalid_password(self):
+        # changing zip code to create a unique property
+        self.data = {
+            "user": {
+                "username": "afonso",
+                "email": "afonso@solano.com",
+                "password": "123" # invalid password
+            },
+            "nCRP": "12345678910", 
+            "bio": "Sou o afonso solano",
+            "genero": "M"
+        }
+        request = self.factory.post(self.url_list, self.data, format='json')
+        # force_authenticate(request, user=self.user) # TODO
+        response = self.view_list(request)
+        self.assertNotEqual(
+            201,
+            response.status_code,
+            msg='Foi retornado 201 Create para uma requisição com senha inválido'
+        )
+     
