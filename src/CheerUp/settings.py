0bd006d7d53bcef7b3d50e4ea49fb2fc6adeeff6
juninhigh <nilvanperes@gmail.com>
2021-04-23 14:15:16 -0300

    Settings #110

--- src/CheerUp/settings.py
+++ src/CheerUp/settings.py
@@ -154,12 +154,9 @@ USE_TZ = True
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
-# Extra places for collectstatic to find static files.
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
+
 
 CORS_ORIGIN_ALLOW_ALL = True
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 
 django_heroku.settings(locals())
